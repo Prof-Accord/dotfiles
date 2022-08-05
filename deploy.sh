5859bc1cb58e8730935c01d15405fbdfb148cf92
Prof-Accord <sh.watanabe94@gmail.com>
2022-08-05 18:24:16 +0900

    create .sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 配置したい設定ファイル
+dotfiles=(
+  .zshrc
+  .zshenv
+  .zprofile
+  .vim
+  .vimrc
+  .git
+  .config
+)
+
+#設定ファイルのシンボリックリンクをホームディレクトリ直下に
+#作成する
+for file in "${dotfiles[@]}"; do
+        ln -svf $file ~/
+done
